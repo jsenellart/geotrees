d88cc4fe2a1cf6109b2721ad72b8d7bcc890a5c1
Jean Senellart <jean@senellart.com>
2025-06-01 14:43:36 +0000

    fix display of the trees from DB and make request more efficient with the use of bbox

--- geotrees/settings.py
+++ geotrees/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'geotrees.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': os.getenv('DB_NAME', 'postgres'),
         'USER': os.getenv('DB_USER', 'postgres'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
--- geotrees/views.py
+++ geotrees/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from core.models import Tree
 from .serializers import TreeGeoSerializer
-
+from django.contrib.gis.geos import Polygon
 from django.shortcuts import render
 
 def map_view(request):
@@ -10,3 +10,17 @@ def map_view(request):
 class TreeGeoViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Tree.objects.all()
     serializer_class = TreeGeoSerializer
+    
+    def get_queryset(self):
+        queryset = Tree.objects.all()
+        bbox = self.request.query_params.get('bbox')
+        
+        if bbox:
+            try:
+                west, south, east, north = map(float, bbox.split(','))
+                bbox_polygon = Polygon.from_bbox((west, south, east, north))
+                queryset = queryset.filter(location__within=bbox_polygon)
+            except (ValueError, TypeError):
+                pass
+                
+        return queryset
