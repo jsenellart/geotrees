b2bfebe2772d1c185b09c5e65ca1cc9e685b54fb
Jean Senellart <jean@senellart.com>
2025-06-01 15:25:23 +0000

    add top bar for loging/logout

--- geotrees/urls.py
+++ geotrees/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 from .views import TreeGeoViewSet
 from .views import map_view
 
@@ -28,4 +29,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('', map_view, name='map'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
